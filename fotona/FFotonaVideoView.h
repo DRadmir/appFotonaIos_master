b9d2a1c1af808b6c69e08e0ee5894018421736d3
Ares <peterg@4egenus.com>
2017-01-11 07:42:12 +0100

    Peter: iPhone add size of bulk downloads

---
+++ fotona/FFotonaVideoView.h
@@ -0,0 +1,13 @@
+//
+//  FFotonaVideoView.h
+//  fotona
+//
+//  Created by Ares on 10/01/17.
+//  Copyright © 2017 4egenus. All rights reserved.
+//
+
+#ifndef FFotonaVideoView_h
+#define FFotonaVideoView_h
+
+
+#endif /* FFotonaVideoView_h */
---
+++ fotona/FFotonaVideoView.m
@@ -0,0 +1,14 @@
+//
+//  FFotonaVideoView.m
+//  fotona
+//
+//  Created by Ares on 10/01/17.
+//  Copyright © 2017 4egenus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FFotonaVideoView.h"
+#import "FDB.h"
+#import "HelperBookmark.h"
+#import "UIColor+Hex.h"
+#import "FDownloadManager.h"
