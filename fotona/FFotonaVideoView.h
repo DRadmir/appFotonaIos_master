f1e0c3e245a045d91dbc6737f3f95e30274b915a
Ares <peterg@4egenus.com>
2017-01-12 08:30:20 +0100

    Peter: added 2 new files

--- fotona/FFotonaVideoView.h
+++ fotona/FFotonaVideoView.h
@@ -2,12 +2,7 @@
 //  FFotonaVideoView.h
 //  fotona
 //
-//  Created by Ares on 10/01/17.
+//  Created by Ares on 11/01/17.
 //  Copyright © 2017 4egenus. All rights reserved.
 //
 
-#ifndef FFotonaVideoView_h
-#define FFotonaVideoView_h
-
-
-#endif /* FFotonaVideoView_h */
--- fotona/FFotonaVideoView.m
+++ fotona/FFotonaVideoView.m
@@ -5,10 +5,3 @@
 //  Created by Ares on 10/01/17.
 //  Copyright © 2017 4egenus. All rights reserved.
 //
-
-#import <Foundation/Foundation.h>
-#import "FFotonaVideoView.h"
-#import "FDB.h"
-#import "HelperBookmark.h"
-#import "UIColor+Hex.h"
-#import "FDownloadManager.h"
